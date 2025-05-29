c0d06d0abf822248c6049a9f5518985a55f1953f
Zeeshan Ahmad <zeeshan>
2025-05-29 22:20:53 +0530

    git init

--- shared/src/Header.jsx
+++ shared/src/Header.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Header = ({color}) => {
   return (
     <header
-      
-      style={{ padding: '1rem', background: {color}, color: 'white' }}>
+     style={{ padding: '1rem', color: {color} }}>
       <h1>🚀 Common Header</h1>
     </header>
   );
